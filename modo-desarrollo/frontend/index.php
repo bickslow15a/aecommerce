236b9c8d4cac44783a8eef9168cd14eb7bbe93df
Chapiama-Laptop <chapiamadurand15@gmail.com>
2019-08-02 17:23:32 -0500

    v3

--- modo-desarrollo/frontend/index.php
+++ modo-desarrollo/frontend/index.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(0);
+//error_reporting(0);
 require_once "controladores/plantilla.controlador.php";
 require_once "controladores/productos.controlador.php";
 require_once "controladores/slide.controlador.php";
